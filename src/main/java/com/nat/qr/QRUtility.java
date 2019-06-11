ae8af3c52a4b72a86ff206741224cabee0d0ad58
mail2pramin <pramin@azurethailand.com>
2019-06-11 17:39:51 +0700

    test

--- src/main/java/com/nat/qr/QRUtility.java
+++ src/main/java/com/nat/qr/QRUtility.java
@@ -64,7 +64,8 @@ public class QRUtility {
 				}
 			}
 		}
-		//build using jenkins
+
+
 		// Base64
 		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
 		ImageIO.write(image, "png", outputStream);
