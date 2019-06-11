b4a737221aa246a345464197ed8bdb799c6ea51d
mail2pramin <pramin@azurethailand.com>
2019-06-11 14:20:56 +0700

    initial commit

---
+++ src/main/java/com/nat/qr/App.java
@@ -0,0 +1,100 @@
+package com.nat.qr;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.NotFoundException;
+import com.google.zxing.WriterException;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+//import com.konylabs.middleware.dataobject.Param;
+import com.nat.qr.QRUtility;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	
+
+    public static void main(String[] args) {
+    	try {
+            
+            // Write
+            String input_acc_id = "0910107494";
+            String pp_acc_id = "";
+            String pp_amount = "";
+            String pp_chksum = "";
+
+            // process input_acc_id
+            if(input_acc_id.length() == 15){ // truemoney e-wallet
+                pp_acc_id = "0315" + input_acc_id;
+            }
+            else if(input_acc_id.length() == 13){ // card-id
+                pp_acc_id = "0213" + input_acc_id;
+            }
+            else if(input_acc_id.length() == 10){ // tel-no
+                pp_acc_id = "01130066" + input_acc_id.substring(1);
+            }
+
+            // process amount
+            String amount = "2000";
+            if(!amount.isEmpty()){
+                pp_amount = String.format("54%02d%s", amount.length(), amount);
+            }
+
+            // build pp string
+            String field_29 = "0016A000000677010111" + pp_acc_id;
+            String pp_str = "000201010211"
+                          + "29" + field_29.length() + field_29                        
+                          + "5802TH"
+                          + pp_amount
+                          + "5303764"
+                          + "6304";
+
+            // process checksum
+            pp_chksum = QRUtility.crc16(pp_str);
+            System.out.println("input for check sum: " + pp_chksum);
+            
+            pp_str += pp_chksum;
+            //System.out.println("PP String : " + pp_str);
+            //String qrCodeData = decodedText; 
+            String qrCodeData = pp_str;
+    		String filePath = "D:\\Docs\\generated.jpg";
+    		String charset = "UTF-8";
+    		Map<EncodeHintType, ErrorCorrectionLevel> hintMap = new HashMap<EncodeHintType, ErrorCorrectionLevel>();
+    		hintMap.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.L);
+    		String imageString = QRUtility.createQRCode(qrCodeData, filePath, charset, hintMap, 200, 200);
+    		System.out.println("imageString = " + imageString);
+    		System.out.println("QR Code image created successfully!");
+    		//System.out.println("QR Code image created successfully!");
+    		// CRC-16
+    		//String sampleTag = decodedText.substring(0, decodedText.length() - 4);
+    		//System.out.println("input for check sum: " + sampleTag);
+    		//System.out.println("input for check sum: " + sampleTag);
+    		//String crc16 = QRUtility.crc16(sampleTag.getBytes());
+    		//LOG.info("crc16 = " + crc16);
+    		
+        	// Read
+            File file = new File("D:\\Docs\\generated.jpg");
+            String decodedText = QRUtility.decodeQr(file);
+            System.out.println("Decoded text = " + decodedText);
+            
+            String s1 = "0011000109200200";
+            s1 = s1.replace("920", "921");
+            System.out.println("replaced no :" + s1); 
+        } catch (IOException e) {
+            System.out.println("Could not decode QR Code, IOException :: " + e.getMessage());
+            //LOG.info("Could not decode QR Code, IOException :: " + e.getMessage());
+        } catch (NotFoundException e) {
+        	System.out.println("No QR Code found in the image");
+        	//LOG.info("No QR Code found in the image");
+		} catch (WriterException e) {
+			e.printStackTrace();
+		}
+    }
+    
+}
---
+++ src/main/java/com/nat/qr/QRUtility.java
@@ -0,0 +1,113 @@
+package com.nat.qr;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.Map;
+
+import javax.imageio.ImageIO;
+import javax.xml.bind.DatatypeConverter;
+
+//import org.apache.log4j.Logger;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.BinaryBitmap;
+import com.google.zxing.LuminanceSource;
+import com.google.zxing.MultiFormatReader;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.NotFoundException;
+import com.google.zxing.Result;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.common.HybridBinarizer;
+//import com.tmb.me.mepay.operation.AccountStatementOperation;
+
+public class QRUtility {
+	public static String decodeQr(File qrCodeimage) throws IOException, NotFoundException {
+        BufferedImage bufferedImage = ImageIO.read(qrCodeimage);
+        LuminanceSource source = new BufferedImageLuminanceSource(bufferedImage);
+        BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(source));
+
+        Result result = new MultiFormatReader().decode(bitmap);
+        return result.getText();
+    }
+	
+	public static String createQRCode(String qrCodeData, String filePath,
+    		String charset, Map hintMap, int qrCodeheight, int qrCodewidth) throws WriterException, IOException {
+    	BitMatrix matrix = new MultiFormatWriter().encode(
+    			new String(qrCodeData.getBytes(charset), charset),
+    			BarcodeFormat.QR_CODE, qrCodewidth, qrCodeheight, hintMap);
+    	// Write to file
+    	MatrixToImageWriter.writeToPath(matrix, filePath.substring(filePath.lastIndexOf('.') + 1), Paths.get(filePath));
+    		
+    	// Image
+		int matrixWidth = matrix.getWidth();
+		BufferedImage image = new BufferedImage(matrixWidth, matrixWidth, BufferedImage.TYPE_INT_RGB);
+		image.createGraphics();
+
+		Graphics2D graphics = (Graphics2D) image.getGraphics();
+		graphics.setColor(Color.WHITE);
+		graphics.fillRect(0, 0, matrixWidth, matrixWidth);
+		// Paint and save the image using the ByteMatrix
+		graphics.setColor(Color.BLACK);
+
+		for (int i = 0; i < matrixWidth; i++) {
+			for (int j = 0; j < matrixWidth; j++) {
+				if (matrix.get(i, j)) {
+					graphics.fillRect(i, j, 1, 1);
+				}
+			}
+		}
+		
+		// Base64
+		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+		ImageIO.write(image, "png", outputStream);
+		//String imageString = "data:image/png;base64," + DatatypeConverter.printBase64Binary(outputStream.toByteArray());
+		String imageString = DatatypeConverter.printBase64Binary(outputStream.toByteArray());
+		//System.out.println("imageString = " + imageString);
+		return imageString;
+    }
+	
+	/*public static String crc16(final byte[] buffer) {
+    	int crc = 0xFFFF;          // initial value
+        int polynomial = 0x1021;   // 0001 0000 0010 0001  (0, 5, 12) 
+
+        // buffer = "123456789".getBytes("ASCII");
+
+        for (byte b : buffer) {
+            for (int i = 0; i < 8; i++) {
+                boolean bit = ((b   >> (7-i) & 1) == 1);
+                boolean c15 = ((crc >> 15    & 1) == 1);
+                crc <<= 1;
+                if (c15 ^ bit) crc ^= polynomial;
+            }
+        }
+
+        crc &= 0xffff;
+        System.out.println("CRC16-CCITT = " + Integer.toHexString(crc).toUpperCase());
+        return Integer.toHexString(crc).toUpperCase();
+    }*/
+	
+	public static String crc16(String input){
+        int crc = 0xFFFF;          // initial value
+        int polynomial = 0x1021;   // 0001 0000 0010 0001  (0, 5, 12)
+        byte[] bytes = input.getBytes();
+        for (byte b : bytes) {
+            for (int i = 0; i < 8; i++) {
+                boolean bit = ((b   >> (7-i) & 1) == 1);
+                boolean c15 = ((crc >> 15    & 1) == 1);
+                crc <<= 1;
+                if (c15 ^ bit) crc ^= polynomial;
+            }
+        }
+        crc &= 0xffff;
+        return String.format("%04x", crc).toUpperCase();
+    }
+
+}
